2878db49d2a6bb38d2ad89e663ec74fe01b96293
Arham~ <53353729+syedarhamali@users.noreply.github.com>
2022-11-27 23:50:37 +0500

    add home button component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,24 +1,29 @@
+import { SearchSharp} from '@mui/icons-material'
+import HomeButtons from '../../screens/HomeButtons';
 import './index.css'
 
 function Header() {
     return (
         <header>
             <div className="header-child">
-                <svg className='brand' viewBox="0 0 36 36" fill="url(#jsc_s_2)" height="40" width="40">
-                    <defs><linearGradient x1="50%" x2="50%" y1="97.0782153%" y2="0%" id="jsc_s_2">
-                        <stop offset="0%" stop-color="#0062E0"></stop>
-                        <stop offset="100%" stop-color="#19AFFF"></stop>
-                    </linearGradient></defs>
-                    <path d="M15 35.8C6.5 34.3 0 26.9 0 18 0 8.1 8.1 0 18 0s18 8.1 18 18c0 8.9-6.5 16.3-15 17.8l-1-.8h-4l-1 .8z"></path>
-                    <path d="M25 23l.8-5H21v-3.5c0-1.4.5-2.5 2.7-2.5H26V7.4c-1.3-.2-2.7-.4-4-.4-4.1 0-7 2.5-7 7v4h-4.5v5H15v12.7c1 .2 2 .3 3 .3s2-.1 3-.3V23h4z"></path>
-                </svg>
-                <input type="text" />
+                <div className='brand'>
+                    <svg viewBox="0 0 36 36" fill="url(#jsc_s_2)" height="40" width="40">
+                        <defs><linearGradient x1="50%" x2="50%" y1="97.0782153%" y2="0%" id="jsc_s_2">
+                            <stop offset="0%" stop-color="#0062E0"></stop>
+                            <stop offset="100%" stop-color="#19AFFF"></stop>
+                        </linearGradient></defs>
+                        <path d="M15 35.8C6.5 34.3 0 26.9 0 18 0 8.1 8.1 0 18 0s18 8.1 18 18c0 8.9-6.5 16.3-15 17.8l-1-.8h-4l-1 .8z"></path>
+                        <path fill='white' d="M25 23l.8-5H21v-3.5c0-1.4.5-2.5 2.7-2.5H26V7.4c-1.3-.2-2.7-.4-4-.4-4.1 0-7 2.5-7 7v4h-4.5v5H15v12.7c1 .2 2 .3 3 .3s2-.1 3-.3V23h4z"></path>
+                    </svg>
+                </div>
+                <img className='sm-brand' width={'120px'} src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Facebook_Logo_%282019%29.svg/1024px-Facebook_Logo_%282019%29.svg.png'alt='logo'/>
+                <div className='header-input'>
+                    <SearchSharp/>
+                    <input type="text" placeholder='Search Facebook'/>
+                </div>
             </div>
             <div className="header-child">
-                <p>1</p>
-                <p>1</p>
-                <p>1</p>
-                <p>1</p>
+                <HomeButtons />
             </div>
             <div className="header-child">
 
---
+++ src/screens/HomeButtons/index.js
@@ -0,0 +1,17 @@
+import StoreOutlinedIcon from '@mui/icons-material/Store';
+import GroupOutlinedIcon from '@mui/icons-material/GroupOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+
+function HomeButtons(){
+    return(
+        <>
+        <HomeOutlinedIcon fontSize='large' />
+        <GroupOutlinedIcon fontSize='large' />
+        <StoreOutlinedIcon fontSize='large' />
+        <NotificationsNoneOutlinedIcon fontSize='large' />
+        </>
+    )
+}
+
+export default HomeButtons
